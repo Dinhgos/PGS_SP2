7289c0447650418309cfe6fcad95a1be95ec496b
Dinhgos <72792073+Dinhgos@users.noreply.github.com>
2022-04-26 19:50:50 +0200

    base xml
    done the basic reading and writing xml

--- main.py
+++ main.py
@@ -1,16 +1,81 @@
-# This is a sample Python script.
+from xml.dom import minidom
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+data = []
 
 
-# Press the green button in the gutter to run the script.
+def write_to_xml():
+    root = minidom.Document()
+
+    xml = root.createElement('Simulation')
+    xml.setAttribute('duration', data[-1][0])
+    root.appendChild(xml)
+
+    bad = root.createElement('blockAverageDuration')
+    bad.setAttribute('totalCount', 'AB')
+    bad.appendChild(root.createTextNode('CD'))
+    xml.appendChild(bad)
+
+    rad = root.createElement('resourceAverageDuration')
+    rad.setAttribute('totalCount', 'EF')
+    rad.appendChild(root.createTextNode('GH'))
+    xml.appendChild(rad)
+
+    faw = root.createElement('ferryAverageWait')
+    faw.setAttribute('trips', 'IJ')
+    faw.appendChild(root.createTextNode('KL'))
+    xml.appendChild(faw)
+
+    workers = root.createElement('Workers')
+    xml.appendChild(workers)
+
+    # TODO - 4 workers
+    worker = root.createElement('Worker')
+    worker.setAttribute('id', 'MN')
+    workers.appendChild(worker)
+
+    # TODO - 4 workers
+    res = root.createElement('resources')
+    res.appendChild(root.createTextNode('OP'))
+    worker.appendChild(res)
+
+    # TODO - 4 workers
+    res = root.createElement('workDuration')
+    res.appendChild(root.createTextNode('QR'))
+    worker.appendChild(res)
+
+    vehicles = root.createElement('Vehicles')
+    xml.appendChild(vehicles)
+
+    # TODO - more
+    vehicle = root.createElement('Vehicle')
+    vehicle.setAttribute('id', 'ST')
+    vehicles.appendChild(vehicle)
+
+    # TODO - more
+    lt = root.createElement('loadTime')
+    lt.appendChild(root.createTextNode('UV'))
+    vehicle.appendChild(lt)
+
+    # TODO - more
+    tt = root.createElement('transportTime')
+    tt.appendChild(root.createTextNode('WX'))
+    vehicle.appendChild(tt)
+
+    xml_str = root.toprettyxml(indent="\t")
+
+    save_path_file = "output.xml"
+
+    with open(save_path_file, "w") as f:
+        f.write(xml_str)
+
+
+def read_input_file():
+    with open("input.txt") as file:
+        for line in file:
+            line = line.rstrip()
+            data.append(line.split(';'))
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    read_input_file()
+    write_to_xml()

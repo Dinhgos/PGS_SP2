c043d5433c4254cbd4c9edd61cdcdd03d2101e5d
Dinhgos <72792073+Dinhgos@users.noreply.github.com>
2022-04-27 10:15:45 +0200

    created lorry and worker array
    created lorry and worker array

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from xml.dom import minidom
 
 data = []
+wor_arr = []
+lor_arr = []
+fer_arr = []
+ores = -1
+blocks = -1
 
 
 def write_to_xml():
@@ -76,6 +81,93 @@ def read_input_file():
             data.append(line.split(';'))
 
 
+def process_input():
+    c_wor = []
+    c_lor = []
+
+    for line in data:
+        match line[1]:
+            case 'Worker':
+                c_wor.append(int(line[2]))
+            case 'Lorry':
+                c_lor.append(int(line[2]))
+
+    mylist1 = list(dict.fromkeys(c_wor))
+    mylist2 = list(dict.fromkeys(c_lor))
+
+    mylist1.sort()
+    mylist2.sort()
+
+    for x in range(0, len(mylist1)):
+        worker = []
+        ore = []
+        block = []
+        worker.append(ore)
+        worker.append(block)
+        wor_arr.append(worker)
+
+    for x in range(0, len(mylist2)):
+        lorry = []
+        load_time = []
+        tp_time = []
+        lorry.append(load_time)
+        lorry.append(tp_time)
+        lor_arr.append(lorry)
+
+    for line in data:
+        match line[1]:
+            case 'Worker':
+                # TODO worker 4 fastest - run the loop first
+                if int(line[2]) >= len(wor_arr):
+                    worker = []
+                    ore = []
+                    block = []
+                    worker.append(ore)
+                    worker.append(block)
+                    wor_arr.append(worker)
+                    if line[3] == 'ore':
+                        wor_arr[int(line[2])][0].append(int(line[4]))
+                    elif line[3] == 'block':
+                        wor_arr[int(line[2])][1].append(int(line[4]))
+                    else:
+                        print('Worker job in time ' + line[0] + ' not recognised.')
+                        exit(1)
+                else:
+                    if line[3] == 'ore':
+                        wor_arr[int(line[2])][0].append(int(line[4]))
+                    elif line[3] == 'block':
+                        wor_arr[int(line[2])][1].append(int(line[4]))
+                    else:
+                        print('Worker job in time ' + line[0] + ' not recognised.')
+                        exit(1)
+
+            case 'Lorry':
+                # TODO lorry fastest
+                if int(line[2]) >= len(lor_arr):
+                    lorry = []
+                    load_time = []
+                    tp_time = []
+                    lorry.append(load_time)
+                    lorry.append(tp_time)
+                    lor_arr.append(lorry)
+                    if line[3] == 'full':
+                        lor_arr[int(line[2])][0].append(int(line[4]))
+                    if line[3] == 'go' or line[3] == 'end':
+                        lor_arr[int(line[2])][1].append(int(line[4]))
+                    else:
+                        print('Lorry job in time ' + line[0] + ' not recognised.')
+                        exit(2)
+                else:
+                    if line[3] == 'full':
+                        lor_arr[int(line[2])][0].append(int(line[4]))
+                    if line[3] == 'go' or line[3] == 'end':
+                        lor_arr[int(line[2])][1].append(int(line[4]))
+                    else:
+                        print('Lorry job in time ' + line[0] + ' not recognised.')
+                        exit(2)
+
+
 if __name__ == '__main__':
     read_input_file()
+    process_input()
     write_to_xml()
